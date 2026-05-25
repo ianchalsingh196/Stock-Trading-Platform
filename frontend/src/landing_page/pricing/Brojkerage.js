37c6dda31968faadd35780613a5b1efea8e5a3f0
Anchal Singh <anchalsingh19654@gmail.com>
2026-05-25 23:12:59 +0530

    Updating the folder of Frontend

---
+++ frontend/src/landing_page/pricing/Brojkerage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BrojKerage() {
+    return (  
+        <h1>Brojkerage</h1>
+    );
+}
+
+export default BrojKerage;
---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+function PricingPage() {
+    return ( 
+        <h1> Pracing Page</h1>
+     );
+}
+
+export default PricingPage;
