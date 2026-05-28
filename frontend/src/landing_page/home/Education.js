4c65d0fc4369418ef0946b44587e5a93065f1713
Anchal Singh <anchalsingh19654@gmail.com>
2026-05-28 17:09:32 +0530

    Updating Pricing.js, Stats.js, Educaton.js

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Education () {
     return ( 
-        <h1>Education</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src='media/images/education.svg' alt='' />
+                </div>
+                <div className='col-6'>
+                    <h1 className='mb-3'>Free and open market education</h1>
+                    <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
+                    <a href='' style={{textDecoration:"none"}}>varsity<i class="fa-solid fa-arrow-right-long"></i></a>
+
+
+                    <p>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
+                    <a href='' style={{textDecoration:"none"}}>Trading Q&A <i class="fa-solid fa-arrow-right-long"></i></a>
+                </div>
+            </div>
+        </div>
      );
 }
 
