aca3d0098f2042c8e07b1189f58af36101475845
Anchal Singh <anchalsingh19654@gmail.com>
2026-05-29 22:07:06 +0530

    Updating Pricing.js,  Educaton.js

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 function Education () {
     return ( 
-        <div className='container'>
+        <div className='container mt-5'>
             <div className='row'>
                 <div className='col-6'>
-                    <img src='media/images/education.svg' alt='' />
+                    <img src='media/images/education.svg' alt='' style={{width:"70%"}}/>
                 </div>
                 <div className='col-6'>
-                    <h1 className='mb-3'>Free and open market education</h1>
-                    <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
+                    <h1 className='mb-3 fs-2'>Free and open market education</h1>
+                    <p className='mt-5'>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
                     <a href='' style={{textDecoration:"none"}}>varsity<i class="fa-solid fa-arrow-right-long"></i></a>
 
 
-                    <p>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
+                    <p className='mt-5'>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
                     <a href='' style={{textDecoration:"none"}}>Trading Q&A <i class="fa-solid fa-arrow-right-long"></i></a>
                 </div>
             </div>
--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <div className='container'>
+        <div className='container mb-5'>
             <div className='row'>
                 <div className='col-4'>
-                    <h1 className='mb-3'>Unbeatable pricing</h1>
+                    <h1 className='mb-3 fs-2'>Unbeatable pricing</h1>
                     <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
                     <a href='' style={{textDecoration:"none"}}>See pricing <i class="fa-solid fa-arrow-right-long"></i></a>
                 </div>
