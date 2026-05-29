772743573664481c0c7758f974b39725dd8cdc27
Anchal Singh <anchalsingh19654@gmail.com>
2026-05-29 22:48:20 +0530

    Updating Navbar.js

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,9 +1,55 @@
 import React from 'react';
 
 function Navbar() {
-    return ( 
-        <h1>Navbar</h1>
-     );
+    return (
+        <nav
+            className="navbar navbar-expand-lg border-bottom"
+            style={{ backgroundColor: "#FFF" }}
+        >
+            <div className="container p-2">
+                <a className="navbar-brand" href="#">
+                    <img
+                        src='media/images/logo.svg'
+                        style={{ width: "25%" }}
+                        alt='Logo'
+                    />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <form className="d-flex" role="search">
+                        <ul className="navbar-nav mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">
+                                    Signup
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Products</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Support</a>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
