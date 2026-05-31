325e9122150763aca8e41f2a5ec1b58e69354f83
Anchal Singh <anchalsingh19654@gmail.com>
2026-05-31 09:51:16 +0530

    Updating Footer.js

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,60 +1,89 @@
 import React from 'react';
 
 function Footer() {
-    return ( 
-        <div className='container'>
-            <div className='row'>
-                <div className='col'>
-                    <img src='media/images/logo.svg' alt='' style={{width:"50%"}}/>
-                    <p>&copy; 2010 - 2024, Zerodha Broking Ltd. All rights reserved.</p>
-                </div>
-                <div className='col'>
-                    <p>Company</p>
-                        <a href='' >About</a> 
-                        <br></br>
-                        <a href='' >Products</a>
-                        <br></br>
-                        <a href='' >Pricing</a>
-                        <br></br>
-                        <a href='' >Referral programme</a>
-                        <br></br>
-                        <a href='' >Careers</a>
-                        <br></br>
-                        <a href='' >Zerodha.tech</a>
-                        <br></br>
-                        <a href='' >Press & media</a>
-                        <br></br>
-                        <a href=''>Zerodha Cares (CSR)</a>
-                </div>
-                <div className='col'>
-                    <p>Support</p>
-                        <a href='' >Contact us</a> 
-                        <br></br>
-                        <a href='' >Support portal</a>
-                        <br></br>
-                        <a href='' > Z-Connect blog</a>
-                        <br></br>
-                        <a href='' >List of charges</a>
-                        <br></br>
-                        <a href='' >Downloads & resources</a>
-                </div>
-                <div className='col'>
-                    <p>Account</p>
-                        <a href='' >Open an accountFund transfer</a> 
+    return (
+        <footer style={{ backgroundColor: "rgb(240, 240, 240)" }}>
+            <div className='container border-top mt-5' >
+                <div className='row mt-5'>
+                    <div className='col'>
+                        <img src='media/images/logo.svg' alt='' style={{ width: "50%" }} />
+                        <p>&copy; 2010 - 2024, Zerodha Broking Ltd. All rights reserved.</p>
+                        <div style={{ display: "flex", flexDirection: "column", gap: "15px", maxWidth: "200px" }}>
+                            {/* First Row (First 4 icons) */}
+                            <div style={{ display: "flex", gap: "15px", alignItems: "center", fontSize: "30px" }}>
+                                <i className="fa-brands fa-x-twitter"></i>
+                                <i className="fa-brands fa-square-facebook"></i>
+                                <i className="fa-brands fa-instagram"></i>
+                                <i className="fa-brands fa-linkedin"></i>
+                            </div>
 
+                            {/* Horizontal Line */}
+                            <hr style={{
+                                border: "none",
+                                height: "2px",
+                                backgroundColor: "#333", /* Dark gray/black - change to #fff if your background is dark */
+                                margin: "5px 0",
+                                width: "100%"
+                            }} />
+
+                            {/* Second Row (Last 3 icons) */}
+                            <div style={{ display: "flex", gap: "15px", alignItems: "center", fontSize: "30px" }}>
+                                <i className="fa-brands fa-youtube"></i>
+                                <i className="fa-solid fa-message"></i>
+                                <i className="fa-brands fa-telegram"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col'>
+                        <p>Company</p>
+                        <a href='' style={{ textDecoration: "none" }} >About</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Products</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }} >Pricing</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }} >Referral programme</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }} >Careers</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }} >Zerodha.tech</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Press & media</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Zerodha Cares (CSR)</a>
+                    </div>
+                    <div className='col'>
+                        <p>Support</p>
+                        <a href='' style={{ textDecoration: "none" }}>Contact us</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Support portal</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }}> Z-Connect blog</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }}>List of charges</a>
+                        <br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Downloads & resources</a>
+                    </div>
+                    <div className='col'>
+                        <p>Account</p>
+                        <a href='' style={{ textDecoration: "none" }}>Open an accountFund transfer</a>
+
+                    </div>
+                </div>
+                <div className='mt-5 text-muted ' style={{ fontSize: "14px" }}>
+                    <p>
+                        Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances Smart Online Dispute Resolution | Grievances Redressal Mechanism
+                    </p>
+                    <p>
+                        Investments in securities market are subject to market risks; read all the related documents carefully before investing. Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month. "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not
+                    </p>
+                    <p>
+                        undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.
+                    </p>
                 </div>
             </div>
-            <p>
-                Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances Smart Online Dispute Resolution | Grievances Redressal Mechanism
-            </p>
-            <p>
-                Investments in securities market are subject to market risks; read all the related documents carefully before investing. Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month. "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not
-            </p>
-            <p>
-                undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.
-            </p>
-        </div>
-     );
+        </footer>
+    );
 }
 
 export default Footer;
