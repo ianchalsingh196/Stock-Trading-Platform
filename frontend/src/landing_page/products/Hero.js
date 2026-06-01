8eda93cc12c051ba483440a0f919a933e69d2ecc
Anchal Singh <anchalsingh19654@gmail.com>
2026-06-01 20:05:52 +0530

    Updating product.js

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        <div className='container'>
+            <div className='text-center mt-5 p-3'>
+                <h1 className='text-muted'>Technology</h1>
+                <h3 className='text-muted mt-3 fs-4'>Sleek, modern, and intuitive trading platforms</h3>
+                <p className='mt-3'>
+                    Check out our <a href='' style={{ textDecoration: "none" }}>investment offerings <i class="fa-solid fa-arrow-right-long"></i></a>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
