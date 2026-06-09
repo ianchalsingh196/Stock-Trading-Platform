d7cd22ae168e5e8861689b8b74dc6325ac7c1bc7
Anchal Singh <anchalsingh19654@gmail.com>
2026-06-09 23:39:29 +0530

    Updating backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
 
 const PORT = process.env.PORT || 3002;
 const MONGO_URI = process.env.MONGO_URI;
@@ -196,6 +196,19 @@ app.get("/allHoldings", async (req, res) => {
 app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionsModel.find({});
   res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
 });
 
 app.listen(PORT, () => {
