b1a634878dc46c8fcc2aa48c85bdb67d94ba1b7a
Anchal Singh <anchalsingh19654@gmail.com>
2026-06-13 21:40:29 +0530

    Updating Backend

--- Backend/index.js
+++ Backend/index.js
@@ -19,6 +19,74 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Signup route
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    console.log(req.body, "req body in signup");
+    const existingUser = await UserModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new UserModel.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Login route
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //find user by email
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    //compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    //generate JWT token
+    const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: "1h"
+    });
+
+    const {password: userPassword, ...userData} = user.toObject();
+
+    res.json({
+      message: "login successfull",
+      token,
+      user: userData,
+    });
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
+
+app.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.user.id).select("-password");
+    // console.log(user, "user in /me", req.user);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 
 app.get("/allHoldings", async (req, res) => {
@@ -30,6 +98,16 @@ app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionsModel.find({});
   res.json(allPositions);
 });
+
+// Get all orders
+// app.get("/allOrders", async (req, res) => {
+//   try {
+//     const orders = await OrdersModel.find({});
+//     res.json(orders);
+//   } catch (err) {
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
 
 app.post("/newOrder", async (req, res) => {
   let newOrder = new OrdersModel({
@@ -44,8 +122,55 @@ app.post("/newOrder", async (req, res) => {
   res.send("Order saved!");
 });
 
+// app.post("/newOrder", async (req, res) => {
+//   const { name, qty, price, mode } = req.body;
+//   console.log(req.body, "new order");
+
+//   try {
+//     let holding = await HoldingsModel.findOne({ name });
+
+//     if (mode === "SELL") {
+//       if (!holding || holding.qty < qty) {
+//         return res.status(400).json({
+//           message: "Insufficient quantity available to SELL",
+//         });
+//       }
+
+//       // SELL: reduce holdings
+//       holding.qty -= qty;
+//       await holding.save();
+//     } else if (mode === "BUY") {
+//       if (!holding) {
+//         holding = new HoldingsModel({ name, qty });
+//       } else {
+//         holding.qty += qty;
+//       }
+//       await holding.save();
+//     }
+
+//     const newOrder = new OrdersModel({
+//       name,
+//       qty,
+//       price,
+//       mode,
+//     });
+//     await newOrder.save();
+
+//     res.status(201).json({
+//       message: "Order placed successfully",
+//       order: newOrder,
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
+
+
+
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    mongoose.connect(MONGO_URI);
-    console.log("DB Connected");
+  console.log(`Server is running on port ${PORT}`);
+  mongoose.connect(MONGO_URI);
+  console.log("DB Connected");
 });
